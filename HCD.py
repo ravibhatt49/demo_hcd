5b789901ca6ddb3b8f52056c9c2374225607e98a
ravibhatt49 <ravibhatt49@gmail.com>
2017-10-13 19:48:11 +0530

    With manage and models

--- HCD.py
+++ HCD.py
@@ -13,21 +13,10 @@ POSTGRES = {
     'port': '5432',
 }
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s'% POSTGRES
 db = SQLAlchemy(app)
 API_KEY = '65c703a61049d706fee3af82bf772df1'
 PASSWORD = '131625b9f4ba96a751aac14ab990e071'
-
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, email):
-        self.email = email
-
-    def __repr__(self):
-        return '<E-mail %r>' % self.email
 
 @app.route('/products', methods=['GET', 'POST'])
 def products():
---
+++ models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, email):
+        self.email = email
+
+    def __repr__(self):
+        return '<E-mail %r>' % self.email
